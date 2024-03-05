0a05d1f6f8e2e560bab2d33b2c20b2c8e443c155
Patrick Huang <huangpatrick16777216@gmail.com>
2024-03-05 12:22:51 -0800

    Replace quotations command.

--- init.lua
+++ init.lua
@@ -929,6 +929,8 @@ vim.keymap.set('n', '<C-\\>', '<cmd>ToggleTerm direction=float<CR>')
 vim.keymap.set('n', '<leader>td', '<cmd>colorscheme tokyonight-night<CR>')
 vim.keymap.set('n', '<leader>tl', '<cmd>colorscheme tokyonight-day<CR>')
 
+vim.keymap.set('n', "<leader>'", '<cmd>s/\'/"/g<CR>')
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
